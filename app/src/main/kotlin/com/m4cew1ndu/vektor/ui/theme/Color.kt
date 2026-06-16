6e96d59d71a3eb98b3aca1f22a38aaac531bca84
m4ce-w1ndu <16584672+m4ce-w1ndu@users.noreply.github.com>
2026-06-16 13:21:46 +0200

    fix: app is now starting after package renaming

---
+++ app/src/main/kotlin/com/m4cew1ndu/vektor/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.m4cew1ndu.vektor.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkBackground = Color(0xFF121318)
+val DarkSurface = Color(0xFF1A1B22)
+val DarkPrimary = Color(0xFF4FD8EB)
+val DarkSecondary = Color(0xFF86D9E7)
+val DarkTertiary = Color(0xFFE2BFCB)
+
+val LightBackground = Color(0xFFF9FAFF)
+val LightSurface = Color(0xFFFFFFFF)
+val LightPrimary = Color(0xFF006874)
+val LightSecondary = Color(0xFF4A6267)
+val LightTertiary = Color(0xFF705761)
